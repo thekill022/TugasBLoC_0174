b30967cafc1e060fe48691654a80f64e71344607
thekill022 <gibfathoni@gmail.com>
2026-04-04 01:46:26 +0700

    membuat class orderSuccess untuk mengambil data transaksi

--- lib/order_state.dart
+++ lib/order_state.dart
@@ -8,3 +8,31 @@ abstract class OrderState extends Equatable {
 class OrderInitial extends OrderState{}
 
 class OrderLoading extends OrderState{}
+
+class OrderSuccess extends OrderState{
+  final String makanan;
+  final String minuman;
+  final int jumlahMakanan;
+  final int jumlahMinuman;
+  final int totalHarga;
+
+  OrderSuccess({
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+    required this.totalHarga
+});
+
+  Map<String, dynamic> toMap() => {
+    'makanan' : makanan,
+    'minuman' : minuman,
+    'jumlahMakanan' : jumlahMakanan,
+    'jumlahMinuman' : jumlahMinuman,
+    'totalHarga' : totalHarga
+  };
+
+  @override
+  List<Object?> get props => [makanan, minuman, jumlahMakanan, jumlahMinuman, totalHarga];
+
+}
