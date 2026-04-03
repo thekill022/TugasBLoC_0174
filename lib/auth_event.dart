1a05219d00cbe8440f2e39ee9371a89674a0629b
thekill022 <gibfathoni@gmail.com>
2026-04-04 00:04:02 +0700

    membuat class untuk menangani data ketika melakukan submit

--- lib/auth_event.dart
+++ lib/auth_event.dart
@@ -4,3 +4,13 @@ abstract class AuthEvent extends Equatable{
   @override
   List<Object?> get props => [];
 }
+
+class LoginSubmitted extends AuthEvent {
+  final String email;
+  final String password;
+
+  LoginSubmitted({required this.email, required this.password});
+
+  @override
+  List<Object?> get props => [email, password];
+}
