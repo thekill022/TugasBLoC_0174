6b0543ac23d3ae58b183cca8b5fd6ca3cf417551
thekill022 <gibfathoni@gmail.com>
2026-04-04 02:42:26 +0700

    menambahkan multiblocprovider di main agar bisa menjalankan lebih dari 1 provider bloc dan menambah route yang diperlukan

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:statemanagement/auth_bloc.dart';
+import 'package:statemanagement/home.dart';
 import 'package:statemanagement/login.dart';
+import 'package:statemanagement/order.dart';
+import 'package:statemanagement/order_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +15,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(create: (_) => AuthBloc()),
+        BlocProvider<OrderBloc>(create: (_) => OrderBloc())
+      ],
+      child: MaterialApp(
+        title: 'Bloc state management app',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        initialRoute: '/login',
+        routes: {
+          "/login": (context) => const LoginPage(),
+          "/home" : (context) => const HomePage(),
+          "/order" : (context) => const OrderPage()
+        },
       ),
-      initialRoute: '/login',
-      routes: {"/login": (context) => const LoginPage()},
     );
   }
 }
