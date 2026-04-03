92cd337337f3a8053318798a70298b69eb7a88ef
thekill022 <gibfathoni@gmail.com>
2026-04-04 02:04:38 +0700

    menghapus variable totalHarga dan method untuk kalkulasi total harga

--- lib/order.dart
+++ lib/order.dart
@@ -14,17 +14,7 @@ class _OrderPageState extends State<OrderPage> {
   final TextEditingController minumanController = TextEditingController();
   final TextEditingController jumlahMakananController = TextEditingController();
   final TextEditingController jumlahMinumanController = TextEditingController();
-  int totalHarga = 0;
   final _formKey = GlobalKey<FormState>();
-
-  void calculateTotalPrice() {
-    int jumlahMakanan = int.tryParse(jumlahMakananController.text) ?? 0;
-    int jumlahMinuman = int.tryParse(jumlahMinumanController.text) ?? 0;
-
-    setState(() {
-      totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman * 5000);
-    });
-  }
 
   @override
   void dispose() {
