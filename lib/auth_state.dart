00dfcfa72ef6ecf7dde5969b63baeb25a66dffb0
thekill022 <gibfathoni@gmail.com>
2026-04-04 00:00:35 +0700

    membat AuthError untuk mengambil pesan error

--- lib/auth_state.dart
+++ lib/auth_state.dart
@@ -20,3 +20,11 @@ class AuthAuthenticated extends AuthState{
 class AuthRegistered extends AuthState{}
 
 class AuthUnauthenticated extends AuthState{}
+
+class AuthError extends AuthState{
+  final String message;
+  AuthError({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
