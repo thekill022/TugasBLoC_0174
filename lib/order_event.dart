0f253e5dc1e5fd59a82036a5ddf5495868891fee
thekill022 <gibfathoni@gmail.com>
2026-04-04 01:53:35 +0700

    menambahkan override dan membuat class OrderReset

--- lib/order_event.dart
+++ lib/order_event.dart
@@ -10,4 +10,16 @@ class OrderSubmitted extends OrderEvent {
   final String minuman;
   final int jumlahMakanan;
   final int jumlahMinuman;
+
+  OrderSubmitted({
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMinuman,
+    required this.jumlahMakanan
+});
+
+  @override
+  List<Object?> get props => [makanan, minuman, jumlahMakanan, jumlahMinuman];
 }
+
+class OrderReset extends OrderEvent {}
